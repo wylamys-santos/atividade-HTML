7bc079c478b4533084ec7e5da63bbe104a6c7969
wylamys santos <jwylamys@gmail.com>
2024-05-09 14:30:54 -0300

    adicionar as paginas contato.html

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,33 +22,37 @@ function enviarFormulario() {
     }
 }
 
-function enviarParaWhatsApp(input) {
-    const nome = document.getElementById('input-nome').value;
-    const email = document.getElementById('input-email').value;
-    const telefone = document.getElementById('input-tel').value;
-    const mensagem = document.getElementById('input-msg').value;
-
-    const texto = `Nome: ${nome}\nE-mail: ${email}\nTelefone: ${telefone}\nMensagem: ${mensagem}`;
-    const textoCodificado = encodeURIComponent(texto);
-    const numeroWhatsApp = '5581982180856'; // Insira o número de telefone do WhatsApp aqui (apenas números)
-    const url = `https://wa.me/${numeroWhatsApp}?text=${textoCodificado}`;
-
-    window.open(url, '_blank');
-
-}
-
-
 function formatPhoneNumber(phoneNumber) {
     // Remove todos os caracteres não numéricos
     phoneNumber = phoneNumber.replace(/\D/g, '');
 
-    // Aplica a máscara (xx) xxxx-xxxx
-    phoneNumber = phoneNumber.replace(/^(\d{2})(\d{4,5})(\d{4})$/, '($1) $2-$3');
+    // Aplica a máscara (XXX) XXX-XXXX
+    phoneNumber = phoneNumber.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
 
     return phoneNumber;
 }
 
 // Exemplo de uso:
-var phoneNumber = "551234567890";
-var phoneNumberFormatted = formatPhoneNumber(phoneNumber);
-console.log(phoneNumberFormatted); // Saída: (55) 12345-6789
+let phoneNumber = '1234567890';
+let formattedPhoneNumber = formatPhoneNumber(phoneNumber);
+console.log(formattedPhoneNumber); // Saída: (123) 456-7890
+
+// Adicionar evento de input ao campo de telefone
+const campoTelefone = document.getElementById('input-tel');
+campoTelefone.addEventListener('input', function () {
+    mascaraTelefone(this);
+});
+
+function enviarParaWhatsApp(){
+    const nome = document.getElementById('input-name').value;
+    const email = document.getElementById('input-email').value;
+    const telefone = document.getElementById('input-tel').value;
+    const mensagem = document.getElementById('input-msg').value;
+
+    const texto = `Nome: ${name}\nE-mail: ${email}\nTelefone: ${telefone}\nMensagem: ${mensagem}`;
+    const textoCodificado = encodeURIComponent(texto);
+    const numeroWhatsApp = '5581982180856'; // Insira o número de telefone do WhatsApp aqui (apenas números)
+    const url = `https://wa.me/${numeroWhatsApp}?text=${textoCodificado}`;
+
+    window.open(url, '_blank');
+}
